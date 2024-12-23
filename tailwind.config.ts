a9d193b2d4a1ac889aa8d63b0d60f5b40df5cef0
Claude White <claude.white2005@gmail.com>
2024-12-23 18:25:42 -0500

    Changed to Daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,21 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [daisyui, typography, forms],
 } satisfies Config;
